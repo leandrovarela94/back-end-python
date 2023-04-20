7b7f006c454f2bf9535e025ad85dbb64a2526259
leandrovarela94 <leandro_varela@live.com>
2023-04-20 17:59:09 -0300

    Alfa - Complete deploy - Test

--- postgres.py
+++ postgres.py
@@ -7,13 +7,14 @@ database_postgres = "postgres"
 host_postgres = "containers-us-west-179.railway.app"
 user_postgres = "postgres"
 password_postgres = "grT1581tkP6rYZy9DXVQ"
+port_postgres = 6796
 
 
 class Postgres(BaseModel):
 
     def connect_postgres():
         conn = psycopg2.connect(
-            database=database_postgres, host=host_postgres, user=user_postgres, password=password_postgres)
+            database=database_postgres, host=host_postgres, user=user_postgres, password=password_postgres, port=port_postgres)
         cur = conn.cursor()
 
         return cur, conn
