dd5c02bc3d1241afef69a23d1896a44ec4a26db5
leandrovarela94 <leandro_varela@live.com>
2023-04-21 10:41:21 -0300

    Contact-Book - bugfix/ATT-01 - Redefine CORS

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from contact_services import ContactSevices
 
 app = FastAPI()
 
+origins = ["https://contact-book-varela.netlify.app/"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
